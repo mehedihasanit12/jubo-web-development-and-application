950213830b938d222c0d38659e6693863dd3b26d
Mehedi Hasan <74129287+mehedihasanit12@users.noreply.github.com>
2024-10-08 11:28:54 +0600

    Add files via upload
    08.10.2024

---
+++ Programming With C/Class 4/program.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+//int main(){
+//    int radius;
+//    double pi = 3.1416,area;
+//
+//    printf("Enter Your Radius: ");
+//    scanf("%d",&radius);
+//
+//    area = pi*radius*radius;
+//
+//    printf("Area is %lf",area);
+//
+//}
+
+//int main(){
+//    float r,pi,area;
+//    pi =3.1416;
+//
+//    printf("Enter the value of radius: ");
+//    scanf("%f",&r);
+//
+//    area = pi*r*r;
+//
+//    printf("Area = %.2f",area);
+//
+//}
+
+//int main(){
+//    float r,pi=3.1416,area;
+//    pi = 3.5;
+//
+//    printf("Enter the value of radius: ");
+//    scanf("%f",&r);
+//
+//    area = pi*r*r;
+//
+//    printf("Area = %.2f",area);
+//
+//}
+
+//Constant use
+
+//int main(){
+//    float r,area;
+//    const float pi =3.1416;
+//    //pi = 10.4;
+//    printf("Enter the value of radius: ");
+//    scanf("%f",&r);
+//
+//    area = pi*r*r;
+//
+//    printf("Area = %.2f",area);
+//
+//}
+
+
+//post,pre increment----------10:41AM
+
+//int main(){
+//    int x = 28,y,z;
+//    y = --x;
+//    printf("The value of x = %d and y = %d\n",x,y);
+//    x=28;
+//    z = x--;
+//    printf("the value of x = %d and z = %d",x,z);
+//
+//}
+
+//Relational operator
+
+int main(){
+    int i=10,j=20,k;
+    k=i<j;
+    printf("%d",k);
+
+}
+
+//Class 3 9:19 AM 08/10/2024
+//Arithmetic Operators ------- radious of a circle is 3 what is the area?
+//                               area = pai*r*r pai=3.1416-------9:15AM
+// format specifier- %d,%lf,%c,%f------9:53AM
+//&-address &r
+//dosomiker por 2 digit -%0.2f
+//value change kora jabe na use korthe hobe constant ----------10:09AM
+//const- read-only variable 'pi' ----const float pi = 3.1416;
+//Operator---------------10:17AM
+//pre-increment-before-------x=4, y=++x; x=5,y=5;
+//post-increment-after-----------10:28AM
+//post,pre increment----------10:41AM
+
+//Relational operators--------------11:07AM
+//>,<, >=,<=,==,!=--------6 ta
+//a=30,b=40, b<=40,b>a,
+
+//Task- 1---picture----------11:23AM
+//Task -2---picture----------11:22AM
+//Task -3---
+//১.সিলিন্ডারের ক্ষেত্রফল,২. ত্রিভুজের ক্ষেত্রফল, ৩.পীথাগোরাসের অতিভূজ, ৪.ট্রপিজিয়ামের ক্ষেত্রফল;
+
