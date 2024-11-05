bf5cd347f4d5446516ea05368d6b281344eb8a3a
mehedi hasan <mehedihasanit12@gmail.com>
2024-11-05 16:16:02 +0600

    05.11.2024 Habib Sir JavaScript Class Form Validation Class 4

---
+++ JavaScript/Class 4/jQuery/JS/script.js
@@ -0,0 +1,162 @@
+//jQuery
+
+
+function fullNameCheck(){
+    var fullNameValue = $('#fulName').val();
+    var regex = /^[a-zA-Z-. ]{2,20}$/;
+    if(regex.test(fullNameValue)){
+        $('#fullNameError').text(" ");
+        return true;
+    } else {
+      $('#fullNameError').text("Full Name should be 2 to 20 Character.");
+      return false; 
+    }
+}
+
+
+$('#fulName').keyup(function(){
+    fullNameCheck();
+});
+
+
+function emailCheck(){
+    var emailAddressValue = $('#emailAddress').val();
+    var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(regex.test(emailAddressValue)){
+        $('#emailError').text(" ");
+        return true;
+    } else {
+      $('#emailError').text("Invalid email address.");
+      return false; 
+    }
+}
+
+
+$('#emailAddress').keyup(function(){
+    emailCheck();
+});
+
+
+function mobileCheck(){
+    var mobileValue = $('#mobile').val();
+    var regex = /^(?:+88|88)?(01[3-9]\d{8})$/;
+    if(regex.test(mobileValue)){
+        $('#mobileError').text(" ");
+        return true;
+    } else {
+      $('#mobileError').text("Invalid Mobile Number.");
+      return false;  
+    }
+}
+
+
+$('#mobile').keyup(function(){
+    mobileCheck();
+});
+
+
+// $('#regForm').submit(function(){
+//     fullNameCheck();
+//     emailCheck();
+//     mobileCheck();
+
+//     if(fullNameCheck()==true && emailCheck()==true && mobileCheck()==true){
+//         return true;
+//     } 
+
+//     return false;
+// })
+
+
+//=========================================================================
+
+
+
+$('#styleOne').click(function(){
+    $('#resDiv').attr('class','style-one');
+});
+
+
+//========================================================================
+
+
+$('#styleOne').click(function(){
+    $('#resDiv').css({
+        'background-color':'black',
+        'height' : '300px',
+        'width' : '400px',
+        'border-radius' : '50%'
+    });
+})
+
+$('#styleDefault').click(function(){
+    $('#resDiv').css({
+        'background-color':'red',
+        'height' : '300px',
+        'width' : '400px',
+        'border-radius' : '0'
+    });
+})
+
+
+
+//=============================================================
+
+
+$('#changeTextBtn').click(function(){ 
+    $('#heading').text('Bangladesh Two');
+});
+
+
+//===============================================================================
+
+
+$('#fullNameBtn').click(function(){ 
+    var firstNameValue = $('#firstName').val();
+    var lastNameValue = $('#lastName').val();
+
+    var fullNameValue = firstNameValue + ' ' + lastNameValue;
+
+    $('#fullName').val(fullNameValue);
+});
+
+
+//===============================================================================
+
+
+
+function smartWatch()
+{
+    var date = new Date();
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var world = 'AM';
+    
+
+    if(hours>12){
+        hello=hours-12;
+        world = 'PM';
+    }
+
+    var myWatch = hello +':'+ minutes+':'+ seconds +' '+world;
+
+    document.getElementById('watchRes').innerText = myWatch;
+}
+
+setInterval(smartWatch, 1000);
+
+
+
+// function test(){
+//     alert('Hello');
+// }
+
+//setInterval(test, 3000);
+//setTimeout(test,3000);
+
+//setInterval();
+//setTimeout();
+
+//console.log(myWatch);
--- Programming With C/Class 10/prime number.c
+++ Programming With C/Class 10/prime number.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    int number, i, j, count,z;
+//    int number, i, j, count;
+//
+//    printf("Enter Your Start Point = ");
+//    scanf("%d", &i);
+//
+//    printf("Enter Your End Point = ");
+//    scanf("%d", &number);
+//
+//
+//    for(i;i<number;i++){
+//        count=0;
+//        for(j=2; j<i; j++){
+//        if(i%j==0){
+//            count++;
+//        }
+//            }
+//    if(count==0){
+//        printf("%d\n", i);
+//    }
+//
+//    }
 
-    printf("Enter Your Start Point = ");
-    scanf("%d", &i);
-
-    printf("Enter Your End Point = ");
-    scanf("%d", &number);
 
 
-    for(i;i<number;i++){
-
-        count=0;
-        for(j=2; j<i; j++){
-        if(i%j==0){
-            count++;
-            z+=count;
-        }
-            }
-    if(count==0){
+    int i;
+    for(i=100;i>=0;i--)
+    {
         printf("%d\n", i);
     }
-
-    }
-
-    printf("Prime Number = %d",z/2);
 
 }
