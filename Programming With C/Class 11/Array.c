0f69e377cdeaf0de042f1058980e916dcfba2585
mehedi hasan <mehedihasanit12@gmail.com>
2024-11-11 09:00:02 +0600

    vokta odikar website design

--- Programming With C/Class 11/Array.c
+++ Programming With C/Class 11/Array.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(){
+
+    int i,sum=0,avarage, marks[5] = {34,65,78,23,65};
+
+    for(i=0;i<5;i++){
+            sum = sum + marks[i];
+
+    }
+    avarage = sum/i+1;
+
+    printf("marks = %d\nAvarage= %d\n",sum,avarage);
+
+}
---
+++ Programming With C/Class 11/size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    double b;
+    float c;
+    char d;
+    int array[4];
+
+    printf("Size of integer = %d\n", sizeof(i));
+    printf("Size of double = %d\n", sizeof(b));
+    printf("Size of float = %d\n", sizeof(c));
+    printf("Size of char = %d\n", sizeof(d));
+    printf("Size of array = %d\n", sizeof(array));
+
+}
